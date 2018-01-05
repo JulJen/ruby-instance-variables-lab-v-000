da6db6cefc50061c3d316bd8167eca125664dd19
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-05 06:17:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,9 +8,14 @@ end
   end
 
   def name
-    @this_dogs_name 
+    @this_dogs_name
   end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
 
 # lassie = Dog.new
 # lassie.name = "Lassie"
