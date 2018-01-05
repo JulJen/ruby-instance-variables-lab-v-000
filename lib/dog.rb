99f3dbe02eae7c52c998b19643c9e2302546286c
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-05 06:18:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,18 +8,3 @@ class Dog
     @this_dogs_name
   end
 end
-
-
-# lassie = Dog.new
-# lassie.name = "Lassie"
-# puts lassie.name
-# end
-
-# describe 'name' do
-#   it 'retrieves the value of an instance variable @this_dogs_name' do
-#     lassie = Dog.new
-#     lassie.instance_variable_set("@this_dogs_name","Lassie")
-#
-#     expect(lassie.name).to eq("Lassie")
-#   end
-# end
