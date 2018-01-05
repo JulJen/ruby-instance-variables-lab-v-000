601fe3f278af33bc63fa6969459c960c9c208702
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-05 06:18:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,14 @@
 class Dog
-  def initialize(dogs_name)
-  @name = name
-end
 
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
 
-  # def name
-  #   @this_dogs_name
-  # end
+  def name
+    @this_dogs_name
+  end
 end
+
 
 # lassie = Dog.new
 # lassie.name = "Lassie"
